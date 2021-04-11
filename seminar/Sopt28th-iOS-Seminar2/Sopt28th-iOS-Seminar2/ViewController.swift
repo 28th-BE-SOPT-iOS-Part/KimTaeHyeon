c9ff8e50f4282f9075f4d094492201eb218a20bc
Taehyeon-Kim <taehyk528@gmail.com>
2021-04-11 19:59:57 +0900

    add: 2주차 세미나
    🌱 기초부터 탄탄하게
    - 레이아웃 기초
    - 오토레이아웃
    - 스토리보드 레퍼런스
    - 제플린, 피그마
    - 탭바컨트롤러·스택뷰·스크롤뷰

---
+++ seminar/Sopt28th-iOS-Seminar2/Sopt28th-iOS-Seminar2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sopt28th-iOS-Seminar2
+//
+//  Created by taehy.k on 2021/04/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
