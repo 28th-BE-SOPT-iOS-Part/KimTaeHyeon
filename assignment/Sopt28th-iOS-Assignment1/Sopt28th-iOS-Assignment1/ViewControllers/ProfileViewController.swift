e2e530a306682132c979f6eddd5031903c998b05
Taehyeon-Kim <taehyk528@gmail.com>
2021-04-22 16:42:33 +0900

    fix: fullscreen modal swipe
    - 기존 풀 스크린 모달 스와이프시 아래에서 위로 스와이프 되는 문제 해결
    - touchPoint.y - initialTouchPoint.y > 0 와 같이 차이를 양수로 설정

--- assignment/Sopt28th-iOS-Assignment1/Sopt28th-iOS-Assignment1/ViewControllers/ProfileViewController.swift
+++ assignment/Sopt28th-iOS-Assignment1/Sopt28th-iOS-Assignment1/ViewControllers/ProfileViewController.swift
@@ -11,6 +11,7 @@ class ProfileViewController: UIViewController {
     
     // MARK: - Variables
     var panGestureRecognizer: UIPanGestureRecognizer?
+    var initialTouchPoint: CGPoint = CGPoint(x: 0, y: 0)
 
     // MARK: - Life Cycle
     override func viewDidLoad() {
@@ -30,27 +31,35 @@ class ProfileViewController: UIViewController {
     // 제스처 시 작동해야 할 메서드 구현
     // 모달을 스와이프로 dismiss하기 위한 메서드
     // - 애니메이션이나 transform에 대한 분석 필요 👨🏻‍🔧❗️
-    @objc func panGestureAction(_ panGesture: UIPanGestureRecognizer) {
-        let translationY = panGesture.translation(in: panGesture.view!).y
-
-        // 팬 제스처의 state를 switch문으로 관리
-        // - 제스처에 대한 이해 필요
-        switch panGesture.state {
+    // - [O] 밑에서 위로 스와이프 되는 이슈 해결
+    @objc func panGestureAction(_ sender: UIPanGestureRecognizer) {
+        let touchPoint = sender.location(in: self.view?.window)
+        
+        // 펜 제스처의 state를 스위치 문으로 관리
+        switch sender.state {
         case .began:
-            break
+            initialTouchPoint = touchPoint
         case .changed:
-            view.transform = CGAffineTransform(translationX: 0, y: translationY)
-        case .ended, .cancelled:
-            if translationY > 160 {
-                dismiss(animated: true, completion: nil)
-            } else {
-                UIView.animate(withDuration: 0.2, animations: {
-                    self.view.transform = CGAffineTransform(translationX: 0, y: 0)
-                })
+            // 아래로 내리는 행위만 가능하도록
+            if touchPoint.y - initialTouchPoint.y > 0 {
+                self.view.frame = CGRect(x: 0,
+                                         y: touchPoint.y - initialTouchPoint.y,
+                                         width: self.view.frame.size.width,
+                                         height: self.view.frame.size.height)
             }
-        case .failed, .possible:
-            break
-        @unknown default:
+        case .ended, .cancelled:
+            // 화면을 1/3이상 내렸을 때만 스와이프 되도록
+            if touchPoint.y - initialTouchPoint.y > (self.view.frame.height / 3) {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                UIView.animate(withDuration: 0.3) {
+                    self.view.frame = CGRect(x: 0,
+                                             y: 0,
+                                             width: self.view.frame.width,
+                                             height: self.view.frame.height)
+                }
+            }
+        default:
             break
         }
     }
