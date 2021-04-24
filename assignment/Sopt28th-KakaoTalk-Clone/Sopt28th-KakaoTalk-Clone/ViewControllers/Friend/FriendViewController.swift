007bbced67958218bff7229e29ca9821491d39ba
Taehyeon-Kim <taehyk528@gmail.com>
2021-04-24 20:12:25 +0900

    add: friend tab 테이블뷰 연결 및 셀 세팅
    - 테이블뷰 기본 세팅
    - 셀별로 각각 호출
    - 더미데이터 안들어간 상태

--- assignment/Sopt28th-KakaoTalk-Clone/Sopt28th-KakaoTalk-Clone/ViewControllers/Friend/FriendViewController.swift
+++ assignment/Sopt28th-KakaoTalk-Clone/Sopt28th-KakaoTalk-Clone/ViewControllers/Friend/FriendViewController.swift
@@ -9,16 +9,76 @@ import UIKit
 
 class FriendViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupTableView()
     }
 
 
-    @IBAction func profileButtonClicked(_ sender: Any) {
-        let profileVC = UIStoryboard(name: "ProfileStoryboard", bundle: nil).instantiateViewController(identifier: "ProfileViewController")
-        profileVC.modalPresentationStyle = .overFullScreen
-        present(profileVC, animated: true, completion: nil)
+//    @IBAction func profileButtonClicked(_ sender: Any) {
+//        let profileVC = UIStoryboard(name: "ProfileStoryboard", bundle: nil).instantiateViewController(identifier: "ProfileViewController")
+//        profileVC.modalPresentationStyle = .overFullScreen
+//        present(profileVC, animated: true, completion: nil)
+//    }
+}
+
+extension FriendViewController {
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
     }
+}
+
+extension FriendViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.section {
+        case 0:
+            return 73
+        case 1:
+            return 50
+        default:
+            return 0
+        }
+    }
+    
+}
+
+extension FriendViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return 10
+        default:
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        switch indexPath.section {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: MyProfileTableViewCell.identifier, for: indexPath) as? MyProfileTableViewCell else { return UITableViewCell() }
+            return cell
+        case 1:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: FriendProfileTableViewCell.identifier, for: indexPath) as? FriendProfileTableViewCell else { return UITableViewCell() }
+            return cell
+        default:
+            return UITableViewCell()
+        }
+    }
+    
+    
 }
