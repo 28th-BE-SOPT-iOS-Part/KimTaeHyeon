dcc4a59d3c19efdcc845575f1164fd2f4f258158
Taehyeon-Kim <taehyk528@gmail.com>
2021-04-24 20:00:09 +0900

    add: friend tab 셀 디자인
    2개가 다르기 때문에 각각 디자인
    - 마이프로필
    - 친구프로필

---
+++ assignment/Sopt28th-KakaoTalk-Clone/Sopt28th-KakaoTalk-Clone/ViewControllers/Friend/Cell/FriendProfileTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  FriendProfileTableViewCell.swift
+//  Sopt28th-iOS-Assignment1
+//
+//  Created by taehy.k on 2021/04/24.
+//
+
+import UIKit
+
+class FriendProfileTableViewCell: UITableViewCell {
+    
+    static let identifier = "FriendProfileTableViewCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
