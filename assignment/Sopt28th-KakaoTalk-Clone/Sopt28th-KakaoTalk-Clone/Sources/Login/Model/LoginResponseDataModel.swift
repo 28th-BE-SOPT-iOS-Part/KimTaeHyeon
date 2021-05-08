f8fa004f64700d4f1098a13287cb41fece3d10b8
Taehyeon-Kim <taehyk528@gmail.com>
2021-05-08 21:17:41 +0900

    add: 로그인 Data Model
    - request data model
    - response data model

---
+++ assignment/Sopt28th-KakaoTalk-Clone/Sopt28th-KakaoTalk-Clone/Sources/Login/Model/LoginResponseDataModel.swift
@@ -0,0 +1,39 @@
+//
+//  LoginResponseDataModel.swift
+//  Sopt28th-iOS-Assignment1
+//
+//  Created by taehy.k on 2021/05/08.
+//
+
+/*
+ {
+     "success": true,
+     "message": "로그인 성공 ^o^",
+     "data": {
+         "UserId": 112,
+         "user_nickname": "sopt_tes",
+         "token": "{ 생략 ...}"
+     }
+ }
+ */
+
+import Foundation
+
+struct LoginResponseDataModel: Codable {
+    let success: Bool
+    let message: String
+    let data: LoginResponse
+}
+
+// MARK: - DataClass
+struct LoginResponse: Codable {
+    let userID: Int
+    let userNickname, token: String
+
+    // CodingKeys 부분?!
+    enum CodingKeys: String, CodingKey {
+        case userID = "UserId"
+        case userNickname = "user_nickname"
+        case token
+    }
+}
