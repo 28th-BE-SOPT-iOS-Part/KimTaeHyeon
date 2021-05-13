ec15c8fa3e805cdcd0432ef0c16b9aed20e6af4b
Taehyeon-Kim <taehyk528@gmail.com>
2021-05-14 01:12:38 +0900

    feat: 일반과제 1, 2 (회원가입, 로그인) 완료
    - 과제1: 회원가입 구현 완료
    - 과제2: 로그인 구현 완료

--- assignment/Sopt28th-KakaoTalk-Clone/Sopt28th-KakaoTalk-Clone/Sources/SignUp/SignUpViewController.swift
+++ assignment/Sopt28th-KakaoTalk-Clone/Sopt28th-KakaoTalk-Clone/Sources/SignUp/SignUpViewController.swift
@@ -25,27 +25,37 @@ class SignUpViewController: UIViewController {
     
     // MARK: - 새로운 카카오계정 만들기 버튼 클릭
     @IBAction func makeNewKakaoAccountButtonClicked(_ sender: Any) {
-        // 뷰컨 인스턴스 생성
-        // guard let confirmVC = storyboard?.instantiateViewController(identifier: "ConfirmViewController") as? ConfirmViewController else { return }
-        // confirmVC.modalPresentationStyle = .overFullScreen
+
+        // 원래는 텍스트 필드에 모두 값이 있을 때만 버튼을 활성화 시키려고 했는데
+        // 알림 중에서 "필요한 값이 존재하지 않습니다."라는 메시지가 있어서 빈 값 일때도 버튼 활성화
         
-        // 홈 화면으로 이동 (탭바 컨트롤러)
-        let homeVC = UIStoryboard(name: "HomeStoryboard", bundle: nil).instantiateViewController(identifier: "HomeTabBarController")
-        homeVC.modalPresentationStyle = .overFullScreen
-        
-        // 텍스트필드 nil값 체크
         guard let emailOrPhoneNumber = emailOrPhoneNumberTextField.text else { return }
         guard let password = passwordTextField.text else { return }
         guard let passwordConfirm = passwordConfirmTextField.text else { return }
         
-        // - 텍스트필드에 모두 값 존재할때만
-        // - 비밀번호와 비밀번호 확인의 값이 같을때만
-        if emailOrPhoneNumber.isEmpty == false && password.isEmpty == false && passwordConfirm.isEmpty == false {
-            if password == passwordConfirm {
-//                confirmVC.emailOrPhoneNumber = emailOrPhoneNumber
-                self.present(homeVC, animated: true, completion: nil)
+        // 비밀번호 같을 때만
+        if password == passwordConfirm {
+            
+            SignUpDataService.shared.postSignUp(email: emailOrPhoneNumber, password: password) { result in
+                
+                switch result {
+                case .success(let message):
+                    if let message = message as? String {
+                        self.didSuccessSignIn(message)
+                    }
+                case .requestErr(let message):
+                    if let message = message as? String {
+                        self.didFailSignIn(message)
+                    }
+                default:
+                    print("ERROR")
+                }
             }
+        } else {
+            self.presentAlert(title: "알림", message: "비밀번호가 같은지 확인해주세요.", okAction: nil)
         }
+        
+
     }
 }
 
@@ -56,5 +66,24 @@ extension SignUpViewController {
         passwordTextField.useUnderline()
         passwordConfirmTextField.useUnderline()
         createKakaoAccountButton.layer.cornerRadius = 4
+    }
+    
+    private func didSuccessSignIn(_ message: String) {
+        self.presentAlert(title: "알림",
+                          message: message) { _ in
+            
+            // 같은 스토리보드 내에 있는 것이 아니라, 다른 스토리보드에 있을 때는 UIStoryboard를 이용해서 인스턴스 생성
+            let homeVC = UIStoryboard(name: "HomeStoryboard", bundle: nil).instantiateViewController(identifier: "HomeTabBarController")
+            homeVC.modalPresentationStyle = .overFullScreen
+            
+            self.present(homeVC, animated: true, completion: nil)
+            
+        }
+    }
+    
+    private func didFailSignIn(_ message: String) {
+        self.presentAlert(title: "알림",
+                          message: message,
+                          okAction: nil)
     }
 }
