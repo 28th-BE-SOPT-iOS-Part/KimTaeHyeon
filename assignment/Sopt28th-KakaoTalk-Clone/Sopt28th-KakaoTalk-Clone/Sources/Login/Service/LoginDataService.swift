d96608fcac9adfb808f416684d0593b1ce2d68eb
Taehyeon-Kim <taehyk528@gmail.com>
2021-05-14 01:42:28 +0900

    feat: 4차과제 중 도전과제1 - 토큰저장
    - service 코드 작성하는 부분에 토큰 저장 코드 추가
    - 확실히 이에 대한 고민 필요
    - 🤔 내부저장소에 토큰이 있는지 없는지 확인하고 저장하는 이 코드를
    - 어디에다가 작성하는 것이 가장 나을지에 대해 고민!!

--- assignment/Sopt28th-KakaoTalk-Clone/Sopt28th-KakaoTalk-Clone/Sources/Login/Service/LoginDataService.swift
+++ assignment/Sopt28th-KakaoTalk-Clone/Sopt28th-KakaoTalk-Clone/Sources/Login/Service/LoginDataService.swift
@@ -56,7 +56,23 @@ struct LoginDataService {
         guard let decodedData = try? decoder.decode(LoginResponseDataModel.self, from: data) else { return .pathErr }
         
         switch statusCode {
-        case 200: return .success(decodedData.message)
+        case 200:
+            // 뷰컨에서 데이터 관련 처리를 직접하기보다는, 여기서 처리하는게 더 나아보임 (주관적)
+            // - 더 나은 방안이 있다면 따르고 싶음! 어렵다..어려워...
+            // 일단 토큰이 있든 없든 내부저장소에서 가져온다.
+            let token = UserDefaults.standard.string(forKey: "token")
+            
+            // 없으면 새로 저장
+            if token == nil {
+                if let token = decodedData.data?.token{
+                    // UserDefaults에 token 저장안되어있을때만, 새롭게 저장
+                    UserDefaults.standard.setValue(token, forKey: "token")
+                    print("토큰 새로저장: \(token)")
+                }
+            } else {
+                print("이미 토큰 존재 \(String(describing: token))")
+            }
+            return .success(decodedData.message)
         case 400: return .requestErr(decodedData.message)
         case 500: return .serverErr
         default: return .networkFail
